1fadedcbaaa8ace0cf8df2a7eece79a02c1e5da9
Marcin.Pudlo <pudlo.marcin@outlook.com>
2019-06-25 18:36:23 +0200

    First application commit / Db migration test

---
+++ CarWashSite/Data/Migrations/20190625163329_DbNameChangeToCarWashSiteDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarWashSite.Data.Migrations
+{
+    public partial class DbNameChangeToCarWashSiteDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
